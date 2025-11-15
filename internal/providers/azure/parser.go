c38012e785f3d8e8444dc10217e9d2895fd97c96
Joel Klabo <joelklabo@gmail.com>
2025-11-15 00:56:42 -0800

    azure: add failing parser tests

---
+++ internal/providers/azure/parser.go
@@ -0,0 +1,8 @@
+package azure
+
+import "github.com/joelklabo/ceye/internal/core"
+
+// ParseAzureRuns converts Azure DevOps build JSON into core.Run structures.
+func ParseAzureRuns(data []byte) ([]core.Run, error) {
+	return nil, nil
+}
